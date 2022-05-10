ed089e75dfadf3b9126d38b08e709a8798db4343
Riley Johnston <rileydanejohnston@gmail.com>
2022-05-09 20:55:56 -0500

    refactor: map function in handleSquareClick becomes updateSquares function

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,60 +3,54 @@ import Square from '../square/Square';
 import { boardDefault } from '../../constants/app-constants';
 
 function App() {
-
-  /* 
-    player 1 will be X
-    player 2 will be O
-  */
 	const [board, setBoard] = useState(boardDefault);
 	const [activePlayer, setActivePlayer] = useState(1);
+  const [turnCount, setTurnCount] = useState(0);
 
-  const changePlayers = () => {
-    /*
-      if p1 is active, activePlayer = p2
-      if p2 is active, activePlayer = p1
-    */
-    const newActivePlayer = activePlayer === 1 ? 2 : 1;
-    setActivePlayer(newActivePlayer);
-  }
-  
+	const changePlayers = () => {
+    // if p1 is active, activePlayer = p2
+    // if p2 is active, activePlayer = p1
+		const newActivePlayer = activePlayer === 1 ? 2 : 1;
+		setActivePlayer(newActivePlayer);
+	};
 
-  const handleSquareClick = (clickId, selected) => {
-    // players can't choose squares that are already chosen
-    if (!selected) {
-    /*
-      find the clicked square
-      update isSelected and player properties
-    */
-      const tempBoard = board.map((square) => {
-        const { id } = square;
-        if (clickId === id) {
-          return {
-            id,
-            isSelected: true,
-            player: activePlayer,
-          };
-        }
-        return square;
-      })
-  
-      setBoard(tempBoard);
-      changePlayers();
-    }
-    
+  const updateSquares = (squareId) => {
+    // find the clicked square
+    // update isSelected and player properties
+    const tempBoard = board.map(square => {
+      const { id } = square;
+      if (squareId === id) {
+        return {
+          id,
+          isSelected: true,
+          player: activePlayer,
+        };
+      }
+      return square;
+    });
+    return tempBoard;
   }
+
+	const handleSquareClick = (clickId, selected) => {
+		// players can't choose squares that are already chosen
+		if (!selected) {
+			const tempBoard = updateSquares(clickId);
+			setBoard(tempBoard);
+			changePlayers();
+		}
+	};
 
 	return (
 		<div className='app'>
 			<ul className='grid'>
-				{board.map((item) => {
+				{board.map(item => {
 					return (
-            <Square
-              key={item.id}
-              squareInfo={item}
-              handleSquareClick={handleSquareClick}
-            />
-          );
+						<Square
+							key={item.id}
+							squareInfo={item}
+							handleSquareClick={handleSquareClick}
+						/>
+					);
 				})}
 			</ul>
 		</div>
